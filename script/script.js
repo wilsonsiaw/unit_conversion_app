19a797a0d1d3fa49e7598f4739c27d1ca2e54536
Wilson Siaw <wilsonsiawse@gmail.com>
2023-08-09 18:18:30 +0200

    Create the conversion functions for each panel

---
+++ script/script.js
@@ -0,0 +1,25 @@
+function temperature() {
+    
+    let celsius = document.getElementById("c").value;
+
+    let fahrenheit = (celsius * 9/5) + 32;
+
+    document.getElementById("f").value = fahrenheit;
+}
+
+function weight() {
+    let kilograms = document.getElementById("kg").value;
+
+    let pounds = kilograms * 2.2;
+
+    document.getElementById("lbs").value = pounds;
+}
+
+function distance() {
+    let kilometers = document.getElementById("km");
+
+    let miles = kilometers * 0.62137;
+
+    document.getElementById("m") = miles;
+}
+
